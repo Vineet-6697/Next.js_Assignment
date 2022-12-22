bfdcc162fe93f58040a7f6799725c508af72a571
Vineet Thakur <bhativineet017@gmail.com>
2022-12-22 17:02:53 +0530

    fixes some bugs

---
+++ pages/api/users/addtocart.js
@@ -0,0 +1,35 @@
+import Users from "../../../models/Users";
+import Userscart from "../../../models/Userscart";
+import connectDB from "../../../middleware/mongoose";   
+
+// PUT API
+
+const handler = async (req, res) => {
+
+
+    if(req.method == 'PUT') {
+     console.log(req.body);
+     try{
+        
+        let temp = new Userscart({
+            userid: req.body.userid,
+            productid: req.body.productid,
+            purchased: false
+        });
+
+        const response = await temp.save();
+
+        // res.status(201).json(response);
+        res.status(201).json({message: 'Product added to the cart', data: response});
+      
+     } catch(err) {
+      res.status(400).json({message: err.message});
+     }
+ 
+    } else {
+     res.status(400).json({message: 'Method not allowed.'});
+    }
+ 
+ }
+ 
+ export default connectDB(handler);
